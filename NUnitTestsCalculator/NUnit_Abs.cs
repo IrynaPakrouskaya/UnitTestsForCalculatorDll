761d88117200684324c09188ea43ee5e735867d2
Iryna Pakrouskaya <Iryna_Pakrouskaya@epam.com>
2017-06-01 20:38:04 +0300

    Add test for Abs method - MSTest

---
+++ NUnitTestsCalculator/NUnit_Abs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitTestsCalculator
+{
+    [TestFixture]
+    public class Abs
+    {
+        [Test]
+        public void testAbsNUnit()
+        {
+            //Arrange
+            CSharpCalculator.Calculator testCalculator = new CSharpCalculator.Calculator();
+
+            //Act
+            double absResult = testCalculator.Abs("-2");
+
+            //Assert
+            Assert.AreEqual(2, absResult);
+        }
+    }
+}
---
+++ UnitTestsCalculator/MSTest_Abs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+namespace UnitTestsCalculator
+{
+    [TestClass]    
+    public class Abs
+    {
+        public CSharpCalculator.Calculator testCalculator;
+        public string inputNumberStringFormat;
+        public double inputNumberDoubleFormat;                   
+
+        [TestInitialize]
+        public void setUpData()
+        {
+            testCalculator = new CSharpCalculator.Calculator(); 
+            inputNumberStringFormat = "-8";
+            inputNumberDoubleFormat = -3.2;
+        }        
+                
+        [TestMethod]
+        public void absTestString(string inputNumberStringFormat)
+        {
+            double absResult = testCalculator.Abs(inputNumberStringFormat);
+            Assert.AreEqual(Math.Abs(Convert.ToDouble(inputNumberStringFormat)), absResult);         
+        }
+
+        [TestMethod]
+        public void absTestDouble(double inputNumberDoubleFormat)
+        {
+            double absResult = testCalculator.Abs(inputNumberDoubleFormat);
+            Assert.AreEqual(Math.Abs(inputNumberDoubleFormat), absResult);
+        }
+
+        [TestCleanup]
+        public void cleanupData()
+        {
+            testCalculator = null;
+            inputNumberStringFormat = null;
+            inputNumberDoubleFormat = 0;
+        }
+    }
+}
