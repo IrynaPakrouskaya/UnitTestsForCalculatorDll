2037496041c4d5f885e341f46dec73ff7c6a861c
Iryna Pakrouskaya <Iryna_Pakrouskaya@epam.com>
2017-06-01 23:20:00 +0300

    Add tests for multiply method

---
+++ MSTestsCalculator/MSTest_Multiply.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestsCalculator
+{
+    [TestClass]
+    public class MSTest_Multiply
+    {
+        private CSharpCalculator.Calculator testCalculator;
+        private double number1Double;
+        private double number2Double;
+
+        [TestInitialize]
+        public void setUpData()
+        {
+            testCalculator = new CSharpCalculator.Calculator();
+            number1Double = 0.8;
+            number2Double = -10.9;
+        }
+
+        [TestMethod]
+        public void multiplyTestDouble()
+        {
+            double resultDivide = testCalculator.Multiply(number1Double, number2Double);
+            double expectedResult = number1Double * number2Double;
+            Assert.AreEqual(expectedResult, resultDivide);
+        }
+
+        [TestCleanup]
+        public void cleanupData()
+        {
+            testCalculator = null;
+            number1Double = 0;
+            number2Double = 0;
+        }
+
+    }
+}
