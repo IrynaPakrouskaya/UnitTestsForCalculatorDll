1cd79b14730faff7f5682cb3c9d29562a5b4b651
Iryna Pakrouskaya <Iryna_Pakrouskaya@epam.com>
2017-06-01 23:15:33 +0300

    Add tests for isPositive method

---
+++ MSTestsCalculator/MSTest_IsPositive.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestsCalculator
+{
+    [TestClass]
+    public class MSTest_IsPositive
+    {
+        private CSharpCalculator.Calculator testCalculator;
+        private string numberString;
+        private double numberDouble;
+
+        [TestInitialize]
+        public void setupData()
+        {
+            testCalculator = new CSharpCalculator.Calculator();
+            numberString = "-8.9";
+            numberDouble = -2.7;
+        }
+
+        [TestMethod]
+        public void isPositiveMSTestString()
+        {
+            Assert.IsFalse(testCalculator.isPositive(numberString));
+        }
+
+        [TestMethod]
+        public void isPositiveMSTestDouble()
+        {
+            Assert.IsFalse(testCalculator.isPositive(numberDouble));
+        }
+
+        [TestCleanup]
+        public void cleanupData()
+        {
+            testCalculator = null;
+            numberString = null;
+            numberDouble = 0;
+        }
+    }
+}
