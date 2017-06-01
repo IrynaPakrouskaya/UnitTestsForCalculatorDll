0258087492f55923e59058f1ba63c80aae9c4136
Iryna Pakrouskaya <Iryna_Pakrouskaya@epam.com>
2017-06-01 23:06:17 +0300

    Add tests for isNegative method

---
+++ MSTestsCalculator/MSTest_IsNegative.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestsCalculator
+{
+    [TestClass]
+    public class MSTest_IsNegative
+    {
+        private CSharpCalculator.Calculator testCalculator;
+        private string numberString;
+        private double numberDouble;
+
+        [TestInitialize]
+        public void setupData()
+        {
+            testCalculator = new CSharpCalculator.Calculator();
+            numberString = "-8.9";
+            numberDouble = -2.7;
+        }
+
+        [TestMethod]
+        public void isNegativeMSTestString()
+        {                      
+            Assert.IsTrue(testCalculator.isNegative(numberString));
+        }
+
+        [TestMethod]
+        public void isNegativeMSTestDouble()
+        {
+            Assert.IsTrue(testCalculator.isNegative(numberDouble));
+        }
+
+        [TestCleanup]
+        public void cleanupData()
+        {
+            testCalculator = null;
+            numberString = null;
+            numberDouble = 0;
+        }
+    }
+}
