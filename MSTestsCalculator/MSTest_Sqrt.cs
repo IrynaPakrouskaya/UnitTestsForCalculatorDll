aa676b66ffa7059ad801196616183810d0bb7925
Iryna Pakrouskaya <Iryna_Pakrouskaya@epam.com>
2017-06-01 23:42:02 +0300

    Add tests for Sqrt method

---
+++ MSTestsCalculator/MSTest_Sqrt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestsCalculator
+{
+    [TestClass]
+    public class MSTest_Sqrt
+    {
+        private CSharpCalculator.Calculator testCalculator;
+        private string inputNumberString;
+        private double inputNumberDouble;
+
+        [TestInitialize]
+        public void setUpData()
+        {
+            testCalculator = new CSharpCalculator.Calculator();
+            inputNumberString = "90.7";
+            inputNumberDouble = -90.7;
+        }
+
+        [TestMethod]
+        public void sqrtMSTestString()
+        {
+            double actualResult = testCalculator.Sqrt(inputNumberString);
+            Assert.AreEqual(Math.Sqrt(Convert.ToDouble(inputNumberString)), actualResult);
+        }
+
+        [TestMethod]
+        public void sqrtMSTestDouble()
+        {
+            double actualResult = testCalculator.Sqrt(inputNumberDouble);
+            Assert.AreEqual(Math.Sqrt(inputNumberDouble), actualResult);
+        }
+
+        [TestCleanup]
+        public void cleanupData()
+        {
+            testCalculator = null;
+            inputNumberDouble = 0;
+            inputNumberString = null;
+        }
+    }
+}
