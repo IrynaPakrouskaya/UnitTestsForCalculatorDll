27364e71f0336b2bb6277b7d0be40057b643166e
Iryna Pakrouskaya <Iryna_Pakrouskaya@epam.com>
2017-06-04 18:18:27 +0300

    Add tests for IsNegative method

---
+++ NUnitTestsCalculator/NUnit_IsNegative.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitTestsCalculator
+{
+    [TestFixture]
+    public class NUnit_IsNegative
+    {
+        private CSharpCalculator.Calculator testCalculator;
+
+        [OneTimeSetUpAttribute]
+        public void SetUpData()
+        {
+            testCalculator = new CSharpCalculator.Calculator();
+        }
+
+        [TestCase(1.2, false)]
+        [TestCase(-3, true)]
+        [TestCase(0, false)]
+        public void IsNegativeNUnitTestDouble(double num, bool expectedResult)
+        {  
+            bool actualResult = testCalculator.isNegative(num);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("1.2", false)]
+        [TestCase("-3", true)]
+        [TestCase("0", false)]
+        public void IsNegativeNUnitTestString(string num, bool expectedResult)
+        {
+            bool actualResult = testCalculator.isNegative(num);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [OneTimeTearDownAttribute]
+        public void CleanupData()
+        {
+            testCalculator = null;
+        }
+    }
+}
