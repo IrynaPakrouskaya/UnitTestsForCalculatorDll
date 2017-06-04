50942112671e6d1dfdf68d61754315e4d0ab4e99
Iryna Pakrouskaya <Iryna_Pakrouskaya@epam.com>
2017-06-04 18:26:11 +0300

    Add tests for IsPositive and Multiply methods

---
+++ NUnitTestsCalculator/NUnit_IsPositive.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitTestsCalculator
+{
+    [TestFixture]
+    public class NUnit_IsPositive
+    {
+        private CSharpCalculator.Calculator testCalculator;
+
+        [OneTimeSetUpAttribute]
+        public void SetUpData()
+        {
+            testCalculator = new CSharpCalculator.Calculator();
+        }
+
+        [TestCase(1.2, true)]
+        [TestCase(-3, false)]
+        [TestCase(0, false)]
+        public void IsPositiveNUnitTestDouble(double num, bool expectedResult)
+        {
+            bool actualResult = testCalculator.isPositive(num);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("1.2", true)]
+        [TestCase("-3", false)]
+        [TestCase("0", false)]
+        public void IsPositiveNUnitTestString(string num, bool expectedResult)
+        {
+            bool actualResult = testCalculator.isPositive(num);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [OneTimeTearDownAttribute]
+        public void CleanupData()
+        {
+            testCalculator = null;
+        }
+    }
+}
---
+++ NUnitTestsCalculator/NUnit_Multiply.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitTestsCalculator
+{
+    [TestFixture]
+    public class NUnit_Multiply
+    {
+        private CSharpCalculator.Calculator testCalculator;
+
+        [OneTimeSetUpAttribute]
+        public void SetUpData()
+        {
+            testCalculator = new CSharpCalculator.Calculator();
+        }
+
+        [TestCase(1.2, 2.3, 2.76)]
+        [TestCase(-3, -2.5, 7.5)]
+        [TestCase(7, 0, 0)]
+        public void MultiplyNUnitTestDouble(double num1, double num2, double expectedResult)
+        {
+            double actualResult = testCalculator.Multiply(num1, num2);
+            Assert.AreEqual(expectedResult, actualResult, 0.0001);
+        }
+
+        [OneTimeTearDownAttribute]
+        public void CleanupData()
+        {
+            testCalculator = null;
+        }
+    }
+}
