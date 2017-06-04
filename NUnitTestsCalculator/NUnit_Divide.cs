a6d8517d1e0ec770990b89b96afb59ee03a77112
Iryna Pakrouskaya <Iryna_Pakrouskaya@epam.com>
2017-06-04 18:03:04 +0300

    Add NUnit tests for Divide method

---
+++ NUnitTestsCalculator/NUnit_Divide.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitTestsCalculator
+{
+    [TestFixture]
+    public class NUnit_Divide
+    {
+        private CSharpCalculator.Calculator testCalculator;
+
+        [OneTimeSetUpAttribute]
+        public void SetUpData()
+        {
+            testCalculator = new CSharpCalculator.Calculator();
+        }
+
+        [TestCase(1.2, 2.3, 0.5217)]
+        [TestCase(-3, -2.5, 1.2)]
+        [TestCase(7, 0, Double.PositiveInfinity)]
+        public void DivideNUnitTestDouble(double num1, double num2, double expectedResult)
+        {
+            double actualResult = testCalculator.Divide(num1, num2);
+            Assert.AreEqual(expectedResult, actualResult, 0.0001);
+        }          
+        
+        [OneTimeTearDownAttribute]
+        public void CleanupData()
+        {
+            testCalculator = null;
+        }
+    }
+}
