8a24408c41223993c34caaad60062059fbf6fb97
Iryna Pakrouskaya <Iryna_Pakrouskaya@epam.com>
2017-06-07 17:37:44 +0300

    Add Data Driven for Multiply method

--- MSTestsCalculator/MSTest_Multiply.cs
+++ MSTestsCalculator/MSTest_Multiply.cs
@@ -7,31 +7,36 @@ namespace UnitTestsCalculator
     public class MSTest_Multiply
     {
         private CSharpCalculator.Calculator testCalculator;
-        private double number1Double;
-        private double number2Double;
+        private TestContext context;
+
+        public TestContext TestContext
+        {
+            get { return context; }
+            set { context = value; }
+        } 
 
         [TestInitialize]
         public void SetUpData()
         {
             testCalculator = new CSharpCalculator.Calculator();
-            number1Double = 0.8;
-            number2Double = -10.9;
         }
 
         [TestMethod]
+        [DeploymentItem("C:\\Users\\Ирина\\Desktop\\TA\\lecture4_UnitTestingFrameworks\\UnitTestsForCalculatorProject\\MSTestsCalculator\\CalculatorTestData.xml")]
+        [DataSource("MultiplyDoubleData")] 
         public void MultiplyMSTestDouble()
         {
-            double resultDivide = testCalculator.Multiply(number1Double, number2Double);
-            double expectedResult = number1Double * number2Double;
-            Assert.AreEqual(expectedResult, resultDivide);
+            double num1 = Convert.ToDouble(context.DataRow["numberOne"]);
+            double num2 = Convert.ToDouble(context.DataRow["numberTwo"]);
+            double expectedResult = Convert.ToDouble(context.DataRow["result"]);
+            double actualresult = testCalculator.Multiply(num1, num2);
+            Assert.AreEqual(expectedResult, actualresult, 0.0001);
         }
 
         [TestCleanup]
         public void CleanupData()
         {
             testCalculator = null;
-            number1Double = 0;
-            number2Double = 0;
         }
 
     }
